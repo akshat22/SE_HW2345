19ff23dd491c9aeaf0731c01cfa428480f3b1ca7
madiha2001 <madiha.mansoori2001@gmail.com>
2022-09-05 18:28:15 -0400

    Update symtest.py

--- test/symtest.py
+++ test/symtest.py
@@ -1,18 +1,18 @@
 import pytest
 
-SymTestObj=Sym()
+symTestObj=Sym()
 def test_sym_add(self):
-    SymTestObj.add("a")
-    SymTestObj.add("a")
-    SymTestObj.add("a")
-    SymTestObj.add("a")
-    SymTestObj.add("b")
-    SymTestObj.add("b")
-    SymTestObj.add("c")
-    assert SymTestObj.total==7, "Addition failed"
+    symTestObj.add("a")
+    symTestObj.add("a")
+    symTestObj.add("a")
+    symTestObj.add("a")
+    symTestObj.add("b")
+    symTestObj.add("b")
+    symTestObj.add("c")
+    assert symTestObj.total==7, "Addition failed"
 def test_sym_mid(self):
-    element=SymTestObj.mid()
+    element=symTestObj.mid()
     assert element=='a',"Mode incorrect"
 def test_sym_div(self):
-    sd=SymTestObj.div()
+    sd=symTestObj.div()
     assert sd>=1.37 and sd<=1.38, "Incorrect Standard Deviation"
